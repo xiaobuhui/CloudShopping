e9da1a765f07c43d27ff8475674b06190f266ba2
xiaobuhuichihuoguo <1136503952@qq.com>
2026-04-22 23:55:54 +0800

    新增品牌接口并测试成功

--- shopping_goods_service/src/main/java/com/itbaizhan/shopping_goods_service/ShoppingGoodsServiceApplication.java
+++ shopping_goods_service/src/main/java/com/itbaizhan/shopping_goods_service/ShoppingGoodsServiceApplication.java
@@ -1,11 +1,15 @@
 package com.itbaizhan.shopping_goods_service;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient //向注册中心注册该服务
@@ -18,4 +22,11 @@ public class ShoppingGoodsServiceApplication {
 		SpringApplication.run(ShoppingGoodsServiceApplication.class, args);
 	}
 
+	// 分页插件
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		return interceptor;
+	}
 }
--- shopping_goods_service/src/main/java/com/itbaizhan/shopping_goods_service/service/BrandServiceImpl.java
+++ shopping_goods_service/src/main/java/com/itbaizhan/shopping_goods_service/service/BrandServiceImpl.java
@@ -1,11 +1,16 @@
 package com.itbaizhan.shopping_goods_service.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itbaizhan.shopping_common.pojo.Brand;
 import com.itbaizhan.shopping_common.service.BrandService;
 import com.itbaizhan.shopping_goods_service.mapper.BrandMapper;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @DubboService
 /*服务层的方法不可分割，要么同时成功，要么同时失败*/
@@ -21,6 +26,39 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public Brand findById(Long id) {
         return brandMapper.selectById(id);
+    }
+
+    @Override
+    public List<Brand> findAll() {
+        return brandMapper.selectList(null);
+    }
+
+    @Override
+    public void add(Brand brand) {
+        brandMapper.insert(brand);
+    }
+
+    @Override
+    public void update(Brand brand) {
+        brandMapper.updateById(brand);
+    }
+
+    @Override
+    public void delete(Long id) {
+        brandMapper.deleteById(id);
+    }
+
+    @Override
+    public Page<Brand> search(Brand brand, int page, int size) {
+        //QueryWrapper = MyBatis-Plus 提供的 “条件构造器”
+        QueryWrapper<Brand> queryWrapper = new QueryWrapper();
+        // 判断品牌名不为空
+        if (brand != null && StringUtils.hasText(brand.getName())){
+            // WHERE name LIKE '%值%'
+            queryWrapper.like("name",brand.getName());
+        }
+        Page<Brand> page1 = brandMapper.selectPage(new Page(page, size), queryWrapper);
+        return page1;
     }
 }
 
